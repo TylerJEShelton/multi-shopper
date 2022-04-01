c040a28b9b3ec5471b5bf7ea9a0bd215ce8e7fd1
Tyler <tylershelton99@gmail.com>
2022-03-31 22:08:55 -0400

    Practice state with TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom';
+import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
 
 const App = () => {
   return (
     <div>
       <h1>Multi Shopper Checklists</h1>
+      <GuestList />
+      <UserSearch />
     </div>
   );
 };
